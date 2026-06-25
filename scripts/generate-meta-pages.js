a1fd589e7404a2669b412f6e9ba0bef50c360f61
Camila Souza <camila.souza@servicenet.com.br>
2026-06-25 16:04:20 -0300

    ajuste

--- scripts/generate-meta-pages.js
+++ scripts/generate-meta-pages.js
@@ -1,27 +1,55 @@
 import fs from 'fs';
 import path from 'path';
 
-const invites = [
-  {
-    slug: 'angel-e-camila',
-    title: 'Casamento Camila & Angel',
-    description: 'Temos a alegria de convidar você para celebrar conosco esse momento especial.',
-    image: 'https://conviteria.shop/images/angel-e-camila/share.jpg'
-  }
-];
+const DOMAIN = 'https://conviteria.shop';
 
+const invitesPath = path.resolve('src/data/invites');
 const distPath = path.resolve('dist');
-const template = fs.readFileSync(path.join(distPath, 'index.html'), 'utf-8');
+const template = fs.readFileSync(
+  path.join(distPath, 'index.html'),
+  'utf8'
+);
 
-invites.forEach(invite => {
+// lê todos os json da pasta
+const files = fs.readdirSync(invitesPath).filter(file => file.endsWith('.json'));
+
+files.forEach(file => {
+  const invite = JSON.parse(
+    fs.readFileSync(path.join(invitesPath, file), 'utf8')
+  );
+
+  const title =
+    invite.pageTitle ||
+    invite.eventTitle ||
+    invite.clientName ||
+    'Convite Digital';
+
+  const description =
+    invite.pageDescription ||
+    invite.description ||
+    '';
+
+  const image =
+    invite.pageImage ||
+    invite.image ||
+    '/default-share.jpg';
+
+  const imageUrl = image.startsWith('http')
+    ? image
+    : `${DOMAIN}${image}`;
+
+  const url = `${DOMAIN}/${invite.slug}`;
+
   const html = template
-    .replaceAll('%TITLE%', invite.title)
-    .replaceAll('%DESCRIPTION%', invite.description)
-    .replaceAll('%IMAGE%', invite.image)
-    .replaceAll('%URL%', `https://conviteria.shop/${invite.slug}`);
+    .replaceAll('%TITLE%', title)
+    .replaceAll('%DESCRIPTION%', description)
+    .replaceAll('%IMAGE%', imageUrl)
+    .replaceAll('%URL%', url);
 
   const folder = path.join(distPath, invite.slug);
 
   fs.mkdirSync(folder, { recursive: true });
   fs.writeFileSync(path.join(folder, 'index.html'), html);
+
+  console.log(`✔ ${invite.slug}`);
 });
